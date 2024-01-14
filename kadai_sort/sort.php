dc4ee9fe10e3ce770b41a5bd159d1dcc8c5dac46
Shinsamago <149288990+Shinsamago@users.noreply.github.com>
2024-01-14 20:14:18 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編(sort)</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        function sort_2way($nums, $order)
+        {
+            if ($order === 'ascending_order') {
+                echo '昇順にソートします。<br>';
+                sort($nums);
+                foreach ($nums as $val) {
+                    echo "$val<br>";
+                }
+            } else {
+                echo '降順にソートします。<br>';
+                rsort($nums);
+                foreach ($nums as $val) {
+                    echo "$val<br>";
+                }
+            }
+        }
+
+        $nums = [15, 4, 18, 23, 10];
+        echo sort_2way($nums, 'ascending_order');
+        echo sort_2way($nums, 'descending_order');
+        ?>
+    </p>
+</body>
+
+</html>
